15083739beb5711388aa76077d38edf0ae0f7e7f
Baptiste Fontaine <b@ptistefontaine.fr>
2019-01-26 22:34:36 +0100

    problem 2018/1.2

--- aoc2018/src/aoc2018/p1.clj
+++ aoc2018/src/aoc2018/p1.clj
@@ -1,14 +1,38 @@
 (ns aoc2018.p1
   (:require [clojure.string :as str]))
 
-(def get-frequency
-  (partial reduce +))
-
-(defn solution
+(defn- parse-file
   [filename]
   (->> filename
        slurp
        str/split-lines
-       (map #(Long/parseLong %))
+       (map #(Long/parseLong %))))
+
+
+(def get-frequency
+  (partial reduce +))
+
+(defn solution1
+  [filename]
+  (->> filename
+       parse-file
        get-frequency
+       println))
+
+
+(defn get-duplicated-frequency
+  [changes]
+  (loop [seen #{0}
+         freq 0
+         changes (cycle changes)]
+    (let [freq (+ freq (first changes))]
+      (if (seen freq)
+        freq
+        (recur (conj seen freq) freq (rest changes))))))
+
+(defn solution2
+  [filename]
+  (->> filename
+       parse-file
+       get-duplicated-frequency
        println))
--- aoc2018/test/aoc2018/p1_test.clj
+++ aoc2018/test/aoc2018/p1_test.clj
@@ -9,3 +9,10 @@
         3 [+1 +1 +1]
         0 [+1 +1 -2]
        -6 [-1 -2 -3]))
+
+(deftest duplicated-frequency
+  (are [freq changes] (= freq (p1/get-duplicated-frequency changes))
+        0 [+1 -1]
+       10 [+3 +3 +4 -2 -4]
+        5 [-6 +3 +8 +5 -6]
+       14 [+7 +7 -2 -7 -4]))
