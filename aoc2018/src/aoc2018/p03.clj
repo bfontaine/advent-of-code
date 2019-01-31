6619962c5060d1dce52524b77e4f16b46f3d5ad9
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2019-01-31 15:01:47 +0100

    2018/3.2

--- aoc2018/src/aoc2018/p03.clj
+++ aoc2018/src/aoc2018/p03.clj
@@ -4,30 +4,59 @@
 
 (defn parse-line
   [line]
-  (let [[x y width height]
-        (->> (re-matches #"#\d+ @ (\d+),(\d+): (\d+)x(\d+)$" line)
+  (let [[claim x y width height]
+        (->> (re-matches #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$" line)
              rest
              (map  #(Long/parseLong %)))]
-    [x y width height]))
+    [claim x y width height]))
 
-(defn solution1
+(defn parse-file
   [filename]
   (->> (slurp filename)
        str/split-lines
-       (map parse-line)
-       ;; {(x, y) -> #
-       (reduce (fn [acc [x y width height]] ; elves
-                  (reduce (fn [acc x] ; x
-                            (reduce (fn [acc y] ; y
-                                      (update acc [x y] (fn [n]
-                                                           (if n
-                                                             (inc n)
-                                                             1))))
-                                    acc
-                                    (range y (+ y height))))
+       (map parse-line)))
 
-                          acc
-                          (range x (+ x width))))
-               {})
+(defn fill-matrix
+  [lines]
+  ;; {(x, y) -> #
+  (reduce (fn [acc [_ x y width height]] ; elves
+            (reduce (fn [acc x] ; x
+                      (reduce (fn [acc y] ; y
+                                (update acc [x y] (fn [n]
+                                                    (if n
+                                                      (inc n)
+                                                      1))))
+                              acc
+                              (range y (+ y height))))
+
+                    acc
+                    (range x (+ x width))))
+          {}
+          lines))
+
+(defn solution1
+  [filename]
+  (->> (parse-file filename)
+       fill-matrix
        (filter (fn [[_ n]] (> n 1)))
        count))
+
+(defn pixels
+  [x y width height]
+  (for [x (range x (+ x width))
+        y (range y (+ y height))]
+    [x y]))
+
+(defn matrix-pixels
+  [m x y width height]
+  (map #(get m %) (pixels x y width height)))
+
+(defn solution2
+  [filename]
+  (let [claims (parse-file filename)
+        matrix (fill-matrix claims)]
+    (->> claims
+         (filter (fn [[claim x y width height]]
+                   (every? #(= 1 %)
+                           (matrix-pixels matrix x y width height))))
+         (map first))))
