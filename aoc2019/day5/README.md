987e296e5d12714686db99e8a29cf2a0fe4644eb
Baptiste Fontaine <b@ptistefontaine.fr>
2019-12-05 22:09:20 +0100

    day 5: problem 1

--- aoc2019/day5/README.md
+++ aoc2019/day5/README.md
@@ -1,1 +1,5 @@
 # Day 5 (Crystal)
+
+## Problem 1
+
+    ./problem1.cr <input.txt>
--- aoc2019/day5/problem1.cr
+++ aoc2019/day5/problem1.cr
@@ -1,16 +1,24 @@
 #!/usr/bin/env crystal
 
-module IntcodeOps
+enum IntcodeModes
+  POSITION = 0
+  IMMEDIATE = 1
+end
+
+enum IntcodeOps
   ADD = 1
   MUL = 2
+  INPUT = 3
+  OUTPUT = 4
   HALT = 99
 end
 
 class IntcodeRunner
   # Initialize a new runner on the given instructions. Note it modifies its
   # argument.
-  def initialize(instructions : Array(Int32))
+  def initialize(instructions : Array(Int32), debug=false)
     @instructions = instructions
+    @debug = debug
 
     @cursor = 0
     @done = false
@@ -32,24 +40,95 @@ class IntcodeRunner
   end
 
   private def run_op
-    op = @instructions[@cursor]
+    instruction = @instructions[@cursor]
+
+    modes, op = instruction.divmod(100)
+
+    debug "op=#{op}, modes=#{modes}"
 
     case op
-    when IntcodeOps::ADD
-      run_op_ptr2 { |a, b| a + b }
-    when IntcodeOps::MUL
-      run_op_ptr2 { |a, b| a * b }
-    when IntcodeOps::HALT
+    when IntcodeOps::ADD.value
+      run_op_ptr2(modes) { |a, b| a + b }
+    when IntcodeOps::MUL.value
+      run_op_ptr2(modes) { |a, b| a * b }
+    when IntcodeOps::INPUT.value
+      run_op_input
+    when IntcodeOps::OUTPUT.value
+      run_op_output(modes)
+    when IntcodeOps::HALT.value
       @done = true
+    else
+      raise "Unknown opcode: #{op}"
     end
   end
 
-  private def run_op_ptr2
-    pos1, pos2, pos3 = @instructions[@cursor+1..@cursor+3]
+  private def get_value(offset, modes)
+    immediate = @instructions[@cursor+offset+1]
+    mode = if modes == 0
+             0  # micro-optimization
+           else
+             # 6789
+             # 3210 <- offset
+             #
+             # offset=2 : 10^2 = 100; 6789/100 = 67; 67 % 10 = 7
+             #
+             m = (modes / (10 ** (offset))).to_i
+             m % 10
+           end
 
-    @instructions[pos3] = \
-      yield @instructions[pos1], @instructions[pos2]
+    case mode
+    when IntcodeModes::IMMEDIATE.value
+      debug "val#{offset}: #{immediate}"
+      immediate
+    when IntcodeModes::POSITION.value
+      val = @instructions[immediate]
+      debug "val#{offset}: [#{immediate}] = #{val}"
+      val
+    else
+      raise "Unknown mode: #{mode}"
+    end
+  end
+
+  private def run_op_ptr2(modes)
+    # OP, a, b, position
+    # position <- block(a, b)
+
+    val1 = get_value(0, modes)
+    val2 = get_value(1, modes)
+
+    pos3 = @instructions[@cursor+3]
+
+    res = yield val1, val2
+
+    debug "op2 val1=#{val1} val2=#{val2} pos3=#{pos3} <- #{res}"
+
+    @instructions[pos3] = res
     @cursor += 4
+  end
+
+  private def run_op_output(modes)
+    # OP, <code>
+    output = get_value(0, modes)
+    debug "output: #{output}"
+    puts output
+    @cursor += 2
+  end
+
+  private def run_op_input
+    # OP, <position>
+    # position <- input (always 1 for now)
+    position = @instructions[@cursor+1]
+    input = 1
+    @instructions[position] = input
+    debug "input: [#{position}]<- #{input}"
+
+    @cursor += 2
+  end
+
+  private def debug(msg)
+    return unless @debug
+    fragment = @instructions[@cursor..@cursor+4]
+    puts "[DEBUG:#{@cursor}: #{fragment.join " "}] #{msg}" if @debug
   end
 end
 
@@ -64,7 +143,5 @@ end
 
 code = File.read(ARGV[0])
 instructions = parse_code(code)
-instructions[1] = 12
-instructions[2] = 2
 runner = IntcodeRunner.new(instructions)
-puts runner.run
+runner.run
