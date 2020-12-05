03f1ef8359fec847d7aa8e918e8b027ddd288b03
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-05 16:48:16 +0100

    day 3, part 2

--- aoc2020/day03/day3.py
+++ aoc2020/day03/day3.py
@@ -1,22 +1,43 @@
-def problem1():
+def check_slope(x_direction, y_direction):
     left_offset = 0
     width = 0
     first = True
     trees = 0
 
     with open("input.txt") as f:
-        for line in f:
+        for y, line in enumerate(f):
             if first:
                 width = len(line.strip())
                 first = False
                 continue
 
-            left_offset = (left_offset + 3) % width
+            if y % y_direction != 0:
+                continue
+
+            left_offset = (left_offset + x_direction) % width
             position = line[left_offset]
             if position == "#":  # tree
                 trees += 1
 
-    print(trees)
+    return trees
+
+
+def problem1():
+    print(check_slope(3, 1))
+
+
+def problem2():
+    total = 1
+    for x, y in (
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2),
+    ):
+        total *= check_slope(x, y)
+
+    print(total)
 
 if __name__ == '__main__':
-    problem1()
+    problem2()
