62ff627aee910bdc88c448b911aa9d963d930f08
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-05 18:48:43 +0100

    day 4, part 2

--- aoc2020/day04/day4.py
+++ aoc2020/day04/day4.py
@@ -13,24 +13,83 @@ MANDATORY_FIELDS = {
 
 
 def parse_passport_fields(s):
-    return re.findall(r"(?:^|\s)([a-z]+):", s)
+    return dict(re.findall(r"(?:^|\s)([a-z]+):(\S+)", s))
 
 
-def valid_password_fields(fields):
-    return not (MANDATORY_FIELDS - set(fields))
+def valid_range(field_value, mini, maxi):
+    return field_value.isdigit() and mini <= int(field_value) <= maxi
 
 
-def problem1():
+def valid_password_fields(fields, problem):
+    field_names = set(fields)
+    all_mandatory_are_present = not (MANDATORY_FIELDS - set(field_names))
+
+    if not all_mandatory_are_present:
+        return False
+
+    if problem == 1:
+        return True
+
+    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    if not valid_range(fields["byr"], 1920, 2002) \
+            or not valid_range(fields["iyr"], 2010, 2020) \
+            or not valid_range(fields["eyr"], 2020, 2030):
+        return False
+
+    # hgt (Height) - a number followed by either cm or in:
+    # If cm, the number must be at least 150 and at most 193.
+    # If in, the number must be at least 59 and at most 76.
+    height = fields["hgt"]
+    height_unit = height[-2:]
+    height_value = height[:-2]
+
+    if height_unit == "cm":
+        if not valid_range(height_value, 150, 193):
+            return False
+    elif height_unit == "in":
+        if not valid_range(height_value, 59, 76):
+            return False
+    else:
+        return False
+
+    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    if not re.match(r"#[0-9a-f]{6}$", fields["hcl"]):
+        return False
+
+    # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    if fields["ecl"] not in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}:
+        return False
+
+    # pid (Passport ID) - a nine-digit number, including leading zeroes.
+    if not re.match(r"\d{9}$", fields["pid"]):
+        return False
+
+    return True
+
+
+def run_problem(problem):
     valid = 0
 
     with open("input.txt") as f:
-        for raw_passport in f.read().split("\n\n"):
+        content = f.read()
+
+        for raw_passport in content.split("\n\n"):
             fields = parse_passport_fields(raw_passport)
-            if valid_password_fields(fields):
+            if valid_password_fields(fields, problem):
                 valid += 1
 
     print(valid)
 
 
+def problem1():
+    run_problem(1)
+
+
+def problem2():
+    run_problem(2)
+
+
 if __name__ == '__main__':
-    problem1()
+    problem2()
