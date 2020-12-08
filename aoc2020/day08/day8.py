fe9afee14e286bff6ed61979a736100241e90ea3
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-08 12:39:03 +0100

    day 8 part 2

--- aoc2020/day08/day8.py
+++ aoc2020/day08/day8.py
@@ -8,15 +8,18 @@ def read_instructions(filename):
 
     return instructions
 
-def problem1(filename):
-    instructions = read_instructions(filename)
+def run_program(instructions):
     acc = 0
     idx = 0
     seen_idx = set()
+    end_idx = len(instructions)
 
     while True:
         if idx in seen_idx:
-            break
+            return "infinite loop", acc
+
+        if idx == end_idx:
+            return "end", acc
 
         seen_idx.add(idx)
 
@@ -31,5 +34,25 @@ def problem1(filename):
 
     print(acc)
 
+
+def problem1(filename):
+    instructions = read_instructions(filename)
+    _, acc = run_program(instructions)
+    print(acc)
+
+def problem2(filename):
+    instructions = read_instructions(filename)
+    for i, (word, n) in enumerate(instructions):
+        if word == "acc":
+            continue
+
+        new_word = "jmp" if word == "nop" else "nop"
+        test_instructions = instructions[:i] + [(new_word, n)] + instructions[i+1:]
+        status, acc = run_program(test_instructions)
+        if status == "end":
+            print(acc)
+            return
+
+
 if __name__ == '__main__':
-    problem1("input.txt")
+    problem2("input.txt")
