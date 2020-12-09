2e59d8057ec9c68c224b3c29039e6ed6090ee206
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-09 13:43:23 +0100

    day 9 part 2

--- aoc2020/day09/day9.py
+++ aoc2020/day09/day9.py
@@ -6,9 +6,7 @@ def read_numbers(filename):
         return [int(line.strip()) for line in f]
 
 
-def problem1(filename, preamble_size=25):
-    numbers = read_numbers(filename)
-
+def get_first_invalid_number(numbers, preamble_size=25):
     sums = deque(maxlen=preamble_size)
 
     for index, number in enumerate(numbers):
@@ -21,8 +19,7 @@ def problem1(filename, preamble_size=25):
                     break
 
             if not found:
-                print(number)
-                break
+                return number
 
         # 2. add the current number to the previous sums
         for offset, index_sums in enumerate(sums):
@@ -32,6 +29,33 @@ def problem1(filename, preamble_size=25):
         sums.append(set())
 
 
+def problem1(filename, preamble_size=25):
+    print(get_first_invalid_number(read_numbers(filename), preamble_size))
+
+
+def problem2(filename, preamble_size=25):
+    numbers = read_numbers(filename)
+    target = get_first_invalid_number(numbers, preamble_size)
+
+    for i, n1 in enumerate(numbers):
+        curr_sum = n1
+        smallest = n1
+        largest = n1
+        for n2 in numbers[i + 1:]:
+            curr_sum += n2
+            if curr_sum > target:
+                break
+
+            if n2 < smallest:
+                smallest = n2
+            elif n2 > largest:
+                largest = n2
+
+            if curr_sum == target:
+                print(sum([smallest, largest]))
+                return
+
+
 if __name__ == '__main__':
-    # problem1("sample.txt", 5)
-    problem1("input.txt")
+    # problem2("sample.txt", 5)
+    problem2("input.txt")
