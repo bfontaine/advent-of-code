4e7afd83d721e3f007469e16225a731616f81db8
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-15 14:02:17 +0100

    day 15, part 2

--- aoc2020/day15/day15.py
+++ aoc2020/day15/day15.py
@@ -1,26 +1,37 @@
 import argparse
 
 
+def run_game(starting_numbers, stop):
+    last_turns = {}
+
+    for i, n in enumerate(starting_numbers[:-1]):
+        last_turns[n] = i + 1
+
+    previous = starting_numbers[-1]
+
+    for turn in range(len(starting_numbers) + 1, stop + 1):
+        previous_turn = turn - 1
+
+        if previous not in last_turns:
+            last_turns[previous] = previous_turn
+            previous = 0
+            continue
+
+        last_turn = last_turns[previous]
+        last_turns[previous] = previous_turn
+
+        previous = previous_turn - last_turn
+
+    return previous
+
+
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("starting_numbers")
-    starting_numbers = p.parse_args().starting_numbers
+    starting_numbers = [int(n) for n in p.parse_args().starting_numbers.split(",")]
 
-    ls = [int(n) for n in starting_numbers.split(",")]
-
-    while len(ls) < 2020:
-        previous = ls[-1]
-
-        try:
-            seen_rindex = ls[-2::-1].index(previous)
-        except ValueError:
-            ls.append(0)
-            continue
-
-        prev_index = len(ls) - seen_rindex - 1
-        ls.append(len(ls) - prev_index)
-
-    print(ls[-1])
+    print("Problem 1:", run_game(starting_numbers, 2020))
+    print("Problem 2:", run_game(starting_numbers, 30000000))
 
 
 if __name__ == '__main__':
