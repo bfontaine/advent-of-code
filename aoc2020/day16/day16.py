e9b46e1adf4082bd2eb6e6a205f4d38dceebc00f
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-16 23:18:06 +0100

    day 16, part 2

--- aoc2020/day16/day16.py
+++ aoc2020/day16/day16.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def parse_ticket(ticket_string):
     return [int(n) for n in ticket_string.split(",")]
 
@@ -25,13 +28,19 @@ def read_notes(filename):
     return classes, ticket, tickets
 
 
+def valid_class(ranges, n):
+    for r in ranges:
+        if n in r:
+            return True
+
+    return False
+
+
 def valid_classes(classes, n):
     class_names = []
     for name, ranges in classes.items():
-        for r in ranges:
-            if n in r:
-                class_names.append(name)
-                break
+        if valid_class(ranges, n):
+            class_names.append(name)
 
     return class_names
 
@@ -47,5 +56,47 @@ def problem1(classes, _ticket, tickets):
     print(error_rate)
 
 
+def valid_ticket(classes, ticket):
+    for n in ticket:
+        if not valid_classes(classes, n):
+            return False
+    return True
+
+
+def problem2(classes, ticket, tickets):
+    valid = defaultdict(set)
+    invalid = defaultdict(set)
+
+    for tk in tickets:
+        if not valid_ticket(classes, tk):
+            continue
+
+        for i, n in enumerate(tk):
+            for name, ranges in classes.items():
+                if valid_class(ranges, n):
+                    valid[i].add(name)
+                else:
+                    invalid[i].add(name)
+
+    known_classes = {}
+    classes_count = len(classes)
+
+    while len(known_classes) != classes_count:
+        for i in list(valid):
+            names = (valid[i] - invalid[i] - set(known_classes))
+            if len(names) == 1:
+                name = list(names)[0]
+                known_classes[name] = i
+                del valid[i]
+
+    p = 1
+
+    for name, i in known_classes.items():
+        if name.startswith("departure"):
+            p *= ticket[i]
+
+    print(p)
+
+
 if __name__ == '__main__':
-    problem1(*read_notes("input.txt"))
+    problem2(*read_notes("input.txt"))
