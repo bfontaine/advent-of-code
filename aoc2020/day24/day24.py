4fc0152b12c05dcf1836fe6aa6caa7996b85a4bc
Baptiste Fontaine <b@ptistefontaine.fr>
2020-12-24 14:34:07 +0100

    day 24, part 2

--- aoc2020/day24/day24.py
+++ aoc2020/day24/day24.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Dict, Tuple, Collection, Set
 
 
 def parse_indication(text):
@@ -51,15 +52,69 @@ def coordinates_from_indication(indication):
     return x, y
 
 
-def problem1(indications):
+def neighbor_coordinates(coordinates):
+    x, y = coordinates
+    return (
+        (x, y - 2),
+        (x + 1, y - 1),
+        (x + 1, y + 1),
+        (x, y + 2),
+        (x - 1, y + 1),
+        (x - 1, y - 1),
+    )
+
+
+Coordinates = Tuple[int, int]
+Tiling = Dict[Coordinates, bool]
+
+
+def next_tiling(tiling: Tiling) -> Tiling:
+    coordinates_to_check: Set[Coordinates] = set()
+    neighbors: Dict[Coordinates, Collection[Coordinates]] = {}
+
+    new_tiling = defaultdict(bool)
+
+    for coordinates in tiling:
+        neighbors[coordinates] = neighbor_coordinates(coordinates)
+        coordinates_to_check.add(coordinates)
+        coordinates_to_check.update(neighbors[coordinates])
+
+    for coordinates in coordinates_to_check:
+        this_neighbors: Collection[Coordinates] = neighbors.get(coordinates) or neighbor_coordinates(coordinates)
+        black_neighbors = len([n for n in this_neighbors if tiling.get(n)])
+        black = tiling.get(coordinates)
+        if black:
+            if black_neighbors > 2 or not black_neighbors:
+                continue  # flip to white
+            # keep it
+            new_tiling[coordinates] = True
+        else:
+            if black_neighbors == 2:
+                new_tiling[coordinates] = True  # flip to black
+
+            # otherwise: keep it
+
+    return new_tiling
+
+
+def count_black_tiles(tiling):
+    return len([t for t in tiling.values() if t])
+
+
+def run(indications):
     tiling = defaultdict(bool)
 
     for indication in indications:
         coordinates = coordinates_from_indication(indication)
         tiling[coordinates] = not tiling[coordinates]
 
-    print(len([t for t in tiling.values() if t]))
+    print("Problem #1:", count_black_tiles(tiling))
+
+    for i in range(100):
+        tiling = next_tiling(tiling)
+
+    print("Problem #2:", count_black_tiles(tiling))
 
 
 if __name__ == '__main__':
-    problem1(read_indications("input.txt"))
+    run(read_indications("input.txt"))
