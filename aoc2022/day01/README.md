b1a50cc59b6b94422b9a4c6b21eac28b96f2cf93
Baptiste Fontaine <b@ptistefontaine.fr>
2022-12-03 12:08:54 +0100

    day 1 part 2

---
+++ aoc2022/day01/README.md
@@ -0,0 +1,6 @@
+# Day 1
+
+## Usage:
+
+    nim compile --run day01.nim 1 input.txt
+    nim compile --run day01.nim 2 input.txt
--- aoc2022/day01/day01.nim
+++ aoc2022/day01/day01.nim
@@ -1,20 +1,34 @@
 import os
+import std/algorithm
 import strutils
 
-if paramCount() != 1:
-  stderr.writeLine("Usage: <program> <input file>")
+if paramCount() != 2:
+  stderr.writeLine("Usage: <program> <problem number> <input file>")
   quit(QuitFailure)
 
-let inputFile = paramStr(1)
+let problemNumber = parseInt(paramStr(1))
+let inputFile = paramStr(2)
+
+if problemNumber != 1 and problemNumber != 2:
+  stderr.writeLine("Problem number must be 1 or 2")
+  quit(QuitFailure)
 
 var currentElf = 0
-var maxElf = 0
+var elves = newSeq[int](0)
 
 for line in lines inputFile:
   if line == "":
-    maxElf = max(maxElf, currentElf)
+    elves.add(currentElf)
     currentElf = 0
   else:
     currentElf += parseInt(line)
 
-echo maxElf
+if currentElf != 0:
+  elves.add(currentElf)
+
+elves.sort(order=SortOrder.Descending)
+
+if problemNumber == 1:
+  echo elves[0]
+else:
+  echo elves[0] + elves[1] + elves[2]
