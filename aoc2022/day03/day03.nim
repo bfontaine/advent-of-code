f1608e15c3443f5db3b9c02edc481342cfae2f51
Baptiste Fontaine <b@ptistefontaine.fr>
2022-12-03 17:45:52 +0100

    day 3 part 2

--- aoc2022/day03/day03.nim
+++ aoc2022/day03/day03.nim
@@ -1,6 +1,8 @@
 import std/sets
 from "../base.nim" import params
 
+type RuckSack = HashSet[char]
+
 proc priority(c: char): int =
   let codePoint = int(c)
   # a (97) .. z -> 1 .. 26
@@ -9,14 +11,29 @@ proc priority(c: char): int =
 
 var totalPriority = 0
 
+var
+  rucksacks: array[0..2, RuckSack]
+  ruckSackIndex = 0
+
 for line in lines params.inputFile:
-  let
-    compartment1 = toHashSet(line[0 .. int(line.len/2-1)])
-    compartment2 = toHashSet(line[int(line.len/2) .. ^1])
+  if params.problemNumber == 1:
+    let
+      compartment1: RuckSack = toHashSet(line[0 .. int(line.len/2-1)])
+      compartment2: RuckSack = toHashSet(line[int(line.len/2) .. ^1])
 
-  var intersection = (compartment1 * compartment2)
-  let common = intersection.pop
+    var intersection = (compartment1 * compartment2)
+    let common = intersection.pop
 
-  totalPriority += priority(common)
+    totalPriority += priority(common)
+  else:
+    rucksacks[ruckSackIndex] = toHashSet(line)
+    if ruckSackIndex == 2:
+      var intersection = (rucksacks[0] * rucksacks[1] * rucksacks[2])
+      let common = intersection.pop
+
+      ruckSackIndex = 0
+      totalPriority += priority(common)
+    else:
+      ruckSackIndex += 1
 
 echo(totalPriority)
