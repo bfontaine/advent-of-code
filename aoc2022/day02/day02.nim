ad8611825858d115e13e19bc7681d32957a2754c
Baptiste Fontaine <b@ptistefontaine.fr>
2022-12-03 16:03:38 +0100

    day 2 part 1

---
+++ aoc2022/day02/day02.nim
@@ -0,0 +1,58 @@
+from strutils import split
+
+from "../base.nim" import problemParams
+
+discard """
+> A for Rock, B for Paper, and C for Scissors
+> X for Rock, Y for Paper, and Z for Scissors
+
+> The score for a single round is the score for the shape you selected
+> (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round
+> (0 if you lost, 3 if the round was a draw, and 6 if you won).
+"""
+
+type Shape = int8
+
+const
+  # Add 1 here since we removed 1 from the Rock/Paper/Scissors values
+  LossScore = 0 + 1
+  DrawScore = 3 + 1
+  WinScore = 6 + 1
+
+  Rock: Shape = 0
+  Paper: Shape = 1
+  Scissors: Shape = 2
+
+
+proc parseShape(x: string): Shape =
+  case x
+  of "A", "X":
+    return Rock
+  of "B", "Y":
+    return Paper
+  of "C", "Z":
+    return Scissors
+  else:
+    assert false
+
+proc score(opponent: string, you: string): int =
+  let
+    opponentShape = parseShape(opponent)
+    yourShape = parseShape(you)
+
+  if opponentShape == yourShape:
+    return yourShape + DrawScore
+  if (opponentShape+1) mod 3 == yourShape:
+    return yourShape + WinScore
+  return yourShape + LossScore
+
+
+let params = problemParams()
+var totalScore = 0
+
+for line in lines params.inputFile:
+  let words = line.split(" ")
+  assert len(words) == 2
+  totalScore += score(words[0], words[1])
+
+echo(totalScore)
