da6aa86030f4a559a952f82445787a272c2ce87b
Baptiste Fontaine <b@ptistefontaine.fr>
2023-12-17 16:36:23 +0100

    day 10 part 2

--- aoc2023/day10.py
+++ aoc2023/day10.py
@@ -1,7 +1,17 @@
-from dataclasses import dataclass, field
-from typing import List, Optional
+import enum
+from dataclasses import dataclass
+from typing import List, Optional, Set, Iterator, Dict
+
+from colorama import Back, Fore, Style
 
 import aoc
+from aoc import c
+
+
+class Orientation(int, enum.Enum):
+    # https://en.wikipedia.org/wiki/Port_and_starboard
+    PORTSIDE = enum.auto()
+    STARBOARD = enum.auto()
 
 
 @dataclass(frozen=True)
@@ -9,24 +19,32 @@ class Direction:
     dx: int
     dy: int
 
+    def opposite(self):
+        return Direction(-self.dx, -self.dy)
+
+    def get_direction_from_orientation(self, orientation: Orientation):
+        idx = DIRECTIONS.index(self)
+        match orientation:
+            case Orientation.PORTSIDE:
+                # portside = -90°
+                return DIRECTIONS[(idx + len(DIRECTIONS) - 1) % len(DIRECTIONS)]
+            case Orientation.STARBOARD:
+                # starboard = +90°
+                return DIRECTIONS[(idx + 1) % len(DIRECTIONS)]
+            case _:
+                raise ValueError(f"Invalid orientation {orientation}")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}<{DIRECTION_SYMBOLS[DIRECTIONS.index(self)]}"
+
 
 NORTH = Direction(0, -1)
 SOUTH = Direction(0, 1)
 EAST = Direction(1, 0)
 WEST = Direction(-1, 0)
 
-_OPPOSITE_DIRECTIONS = {
-    NORTH: SOUTH,
-    SOUTH: NORTH,
-    WEST: EAST,
-    EAST: WEST,
-}
-
-DIRECTIONS = tuple(_OPPOSITE_DIRECTIONS)
-
-
-def get_opposite_direction(direction: Direction):
-    return _OPPOSITE_DIRECTIONS[direction]
+DIRECTIONS = (NORTH, EAST, SOUTH, WEST)
+DIRECTION_SYMBOLS = "NESW"
 
 
 @dataclass(frozen=True)
@@ -37,18 +55,21 @@ class Position:
     def go_to(self, direction: Direction):
         return Position(x=self.x + direction.dx, y=self.y + direction.dy)
 
+    def iter_neighbors(self):
+        return (self.go_to(direction) for direction in DIRECTIONS)
 
+
+@dataclass(frozen=True)
 class Tile:
-    pass
+    symbol: str
 
 
-STARTING_TILE = Tile()
-GROUND = Tile()
+STARTING_TILE = Tile('S')
+GROUND = Tile('.')
 
 
 @dataclass(frozen=True)
 class Pipe(Tile):
-    symbol: str
     direction1: Direction
     direction2: Direction
 
@@ -88,80 +109,213 @@ def parse_tile(sym: str) -> Tile:
             raise ValueError(f"Unrecognized symbol {sym}")
 
 
-@dataclass
 class Map:
-    tiles: List[List[Tile]] = field(default_factory=list)
-    _starting_position: Optional[Position] = None
+    def __init__(self, tiles: List[List[Tile]]) -> None:
+        self.tiles = tiles
+
+        self._loop: Optional[List[Position]] = None
+        self._side_tiles: Dict[Orientation, Set[Position]] = {}
+
+        self.starting_position = self._get_starting_position()
+        self._set_loop()
 
     @classmethod
     def from_text(cls, text: str):
-        m = cls()
+        tiles: List[List[Tile]] = []
         for line in text.splitlines(keepends=False):
-            m.tiles.append([parse_tile(c) for c in line])
-        return m
+            tiles.append([parse_tile(char) for char in line])
+        return cls(tiles=tiles)
 
     @property
-    def starting_position(self):
-        if self._starting_position is None:
-            for y, row in enumerate(self.tiles):
-                for x, tile in enumerate(row):
-                    if tile == STARTING_TILE:
-                        self._starting_position = Position(x, y)
-                        return self._starting_position
-            raise RuntimeError("Can't find starting position")
-        return self._starting_position
+    def width(self):
+        return len(self.tiles[0])
+
+    @property
+    def height(self):
+        return len(self.tiles)
+
+    def get_loop(self):
+        assert self._loop is not None
+        return self._loop
+
+    def get_side_tiles(self):
+        return self._side_tiles
+
+    def iter_positions(self) -> Iterator[Position]:
+        """
+        Yield all positions for the map. Positions closer to the edges are yielded first.
+        """
+        min_y = 0
+        max_y = self.height - 1
+        min_x = 0
+        max_x = self.width - 1
+        while min_y <= max_y and min_x <= max_x:
+            for x in range(min_x, max_x + 1):
+                yield Position(x, min_y)
+                if min_y < max_y:
+                    yield Position(x, max_y)
+
+            min_y += 1
+            max_y -= 1
+
+            for y in range(min_y, max_y + 1):
+                yield Position(min_x, y)
+                if min_x < max_x:
+                    yield Position(max_x, y)
+
+            min_x += 1
+            max_x -= 1
+
+    def _get_starting_position(self):
+        for position in self.iter_positions():
+            if self.get(position) == STARTING_TILE:
+                return position
+        raise RuntimeError("Can't find starting position")
+
+    def is_outside(self, position: Position):
+        return position.x < 0 or position.y < 0 or position.x >= self.width or position.y >= self.height
 
     def get(self, position: Position):
-        try:
+        if not self.is_outside(position):
             return self.tiles[position.y][position.x]
-        except IndexError:
-            return None
+        return None
 
     def get_connected_pipe(self, origin: Position, direction: Direction):
         """
         Look for a tile going from ``origin`` to ``direction``, and if there is a pipe there that’s connected to
-        ``origin``, return a tuple of ``(new_position, next_direction)`` where ``new_position`` is the position of the
-        pipe tile and ``next_direction`` is the next direction to use to follow the pipe.
+        ``origin``, return a tuple of ``(new_position, pipe, next_direction)`` where ``new_position`` is the position of
+        the pipe tile and ``next_direction`` is the next direction to use to follow the pipe.
         """
         new_position = origin.go_to(direction)
         pipe = self.get(new_position)
         if not isinstance(pipe, Pipe):
             return None
 
-        direction_to_origin = get_opposite_direction(direction)
+        direction_to_origin = direction.opposite()
 
         if pipe.is_connected(direction_to_origin):
-            return new_position, pipe.opposite_end(direction_to_origin)
+            return new_position, pipe, pipe.opposite_end(direction_to_origin)
 
         return None
 
-    def get_initial_pipe(self) -> tuple[Position, Direction]:
+    def get_initial_pipe(self) -> tuple[Position, Pipe, Direction]:
         for direction in DIRECTIONS:
             if initial_pipe := self.get_connected_pipe(self.starting_position, direction):
                 return initial_pipe
         raise RuntimeError("Can't find initial pipe")
 
+    def _set_loop(self) -> None:
+        loop_positions = [self.starting_position]
+
+        side_tiles: Dict[Orientation, Set[Position]] = {
+            orientation: set() for orientation in list(Orientation)
+        }
+
+        new_position, pipe, next_direction = self.get_initial_pipe()
+
+        while new_position != self.starting_position:
+            loop_positions.append(new_position)
+
+            prev_direction = pipe.opposite_end(next_direction).opposite()
+
+            for orientation in Orientation:
+                for direction in (prev_direction, next_direction):
+                    orientation_direction = direction.get_direction_from_orientation(orientation)
+                    orientation_position = new_position.go_to(orientation_direction)
+                    if not self.is_outside(orientation_position):
+                        side_tiles[orientation].add(orientation_position)
+
+            next_pipe = self.get_connected_pipe(new_position, next_direction)
+            if next_pipe is None:
+                assert new_position.go_to(next_direction) == self.starting_position
+                break
+
+            new_position, pipe, next_direction = next_pipe
+
+        for orientation_tiles in side_tiles.values():
+            orientation_tiles.difference_update(loop_positions)
+
+        self._loop = loop_positions
+        self._side_tiles = side_tiles
+
+        for side_tiles_set in self._side_tiles.values():
+            self._expand_side_tiles(side_tiles_set)
+
+    def _expand_side_tiles(self, tiles: Set[Position]):
+        loop_positions = set(self.get_loop())
+        while True:
+            size = len(tiles)
+            for position in list(tiles):
+                # Not very efficient
+                for neighbor in position.iter_neighbors():
+                    if self.is_outside(neighbor):
+                        continue
+
+                    if neighbor not in loop_positions:
+                        tiles.add(neighbor)
+
+            if len(tiles) == size:
+                break
+
+    def get_outside_positions(self) -> Set[Position]:
+        """Return all positions that are directly connected to an edge of the map."""
+        loop_positions = set(self.get_loop())
+        outside_positions: Set[Position] = set()
+
+        for position in self.iter_positions():
+            if position in loop_positions:
+                continue
+
+            if any(
+                    self.is_outside(neighbor) or neighbor in outside_positions
+                    for neighbor in position.iter_neighbors()
+            ):
+                outside_positions.add(position)
+                continue
+
+        return outside_positions
+
+    def show(self) -> None:
+        loop_positions = set(self.get_loop())
+
+        for y, row in enumerate(self.tiles):
+            for x, tile in enumerate(row):
+                position = Position(x, y)
+
+                colors: List[str] = []
+
+                if position in self._side_tiles[Orientation.PORTSIDE]:
+                    colors.extend((Back.RED, Fore.WHITE))
+                elif position in self._side_tiles[Orientation.STARBOARD]:
+                    colors.extend((Back.GREEN, Fore.WHITE))
+
+                if position not in loop_positions:
+                    colors.append(Style.DIM)
+
+                print(c(tile.symbol, *colors) if colors else tile.symbol, end="")
+            print()
+
 
 def problem1(text: str):
-    m = Map.from_text(text)
-    initial_position = m.starting_position
-
-    new_position, next_direction = m.get_initial_pipe()
-    steps = 1
-    while new_position != initial_position:
-        next_pipe = m.get_connected_pipe(new_position, next_direction)
-        steps += 1
-        if next_pipe is None:
-            assert new_position.go_to(next_direction) == initial_position
-            break
-
-        new_position, next_direction = next_pipe
-
-    return steps // 2
+    return len(Map.from_text(text).get_loop()) // 2
 
 
 def problem2(text: str):
-    raise NotImplementedError()
+    m = Map.from_text(text)
+
+    if all(not s for s in m.get_side_tiles().values()):
+        return 0
+
+    outside_positions = m.get_outside_positions()
+
+    side_tiles_sets = list(m.get_side_tiles().values())
+    # If one side overlaps outside tiles, return the size of the other
+    for i, side_tiles_set in enumerate(side_tiles_sets):
+        if side_tiles_set & outside_positions:
+            return len(side_tiles_sets[(i + 1) % 2])
+
+    # There are no outside tiles
+    return sum(map(len, side_tiles_sets))
 
 
 if __name__ == '__main__':
--- aoc2023/tests/test_day10.py
+++ aoc2023/tests/test_day10.py
@@ -1,7 +1,8 @@
+import clj
 import pytest
 
-from day10 import problem1, problem2, Position, Map, STARTING_TILE, GROUND
-from aoc import assert_examples
+from day10 import problem1, problem2, Position, Map, STARTING_TILE, GROUND, Orientation, NORTH, WEST, EAST, SOUTH
+from aoc import assert_examples, get_input_data
 
 
 @pytest.fixture()
@@ -29,7 +30,175 @@ def test_starting_position(simple_sample1):
 
 
 def test_problem1_examples():
-    assert_examples(problem1)
+    assert_examples(problem1, examples=(0, 1))
 
-# def test_problem2_examples():
-#     assert_examples(problem2)
+
+@pytest.mark.parametrize("direction,orientation,expected", [
+    (NORTH, Orientation.PORTSIDE, WEST),
+    (NORTH, Orientation.STARBOARD, EAST),
+    (SOUTH, Orientation.PORTSIDE, EAST),
+    (SOUTH, Orientation.STARBOARD, WEST),
+    (WEST, Orientation.PORTSIDE, SOUTH),
+    (WEST, Orientation.STARBOARD, NORTH),
+    (EAST, Orientation.PORTSIDE, NORTH),
+    (EAST, Orientation.STARBOARD, SOUTH),
+])
+def test_get_direction_from_orientation(direction, orientation, expected):
+    assert direction.get_direction_from_orientation(orientation) == expected
+
+
+@pytest.mark.parametrize("text", [
+    # even x odd
+    """
+......
+.F--7.
+.|..|.
+.S--J.
+......
+""",
+    # Note: ensure these don't have (1,1) on the edge
+    # even x even
+    "S--7\n|..|\n|..|\nL--J",
+    # odd x even
+    "S-7\n|.|\n|.|\nL-J",
+    # odd x odd
+    "S-7\n|.|\nL-J",
+])
+def test_iter_positions(text):
+    text = text.strip()
+    m = Map.from_text(text)
+    positions = list(m.iter_positions())
+    assert len(positions) == len(text.replace("\n", ""))
+
+    edge_size = m.width * 2 + m.height * 2 - 4
+    edge_positions = set(clj.take(edge_size, positions))
+    assert Position(0, 0) in edge_positions
+    assert Position(m.width - 1, m.height - 1) in edge_positions
+    assert Position(1, 1) not in edge_positions
+
+
+@pytest.mark.parametrize("text,expected", [
+    ("S7\nLJ", 0),
+    ("""
+F----7
+|FS-7|
+||..||
+LJ..LJ
+""".strip(), 4),
+])
+def test_get_outside_positions(text, expected):
+    assert len(Map.from_text(text).get_outside_positions()) == expected
+
+
+@pytest.mark.parametrize("text", [
+    # 4-tiles loop with different starting positions
+    "S7\nLJ",
+    "FS\nLJ",
+    "F7\nSJ",
+    "F7\nLS",
+
+    """
+    S-7
+    L-J
+    """,
+    # inverted U
+    """
+    -F--S|
+    -|F7|-
+    JLJLJF
+    """,
+])
+def test_problem2_simple_empty_loops(text):
+    assert problem2(text.strip().replace(" ", "")) == 0
+
+
+def test_problem2_empty_loop():
+    text = """
+F----7
+|F---J
+|L---7
+|F---J
+|L---7
+L----J
+    """.strip()
+
+    for i, c in enumerate(text):
+        if c == "\n":
+            continue
+
+        text_with_starting_position = text[:i] + "S" + text[i + 1:]
+        assert problem2(text_with_starting_position) == 0
+
+
+@pytest.mark.parametrize("text,expected", [
+    (
+            """
+F----7
+|F---J
+||F--7
+|||..|
+|LJ..S
+|F---J
+|L---7
+L----J
+            """, 4),
+])
+def test_problem2_simple_loops(text, expected):
+    assert problem2(text.strip().replace(" ", "")) == expected
+
+
+@pytest.mark.parametrize("text,expected", [
+    (
+            """
+            S-7
+            |.|
+            L-J
+            """, 1),
+    (
+            """
+            F--7
+            |--|
+            ||J|
+            L-SJ
+            """, 4),
+    (
+            # one inside tile with only edges on its side
+            """
+            ..F-S
+            F-JFJ
+            L7.L7
+            FJF7|
+            L-JLJ
+            """, 1),
+    (
+            # tiles outside that don't touch the loop
+            """
+            ..........
+            .S------7.
+            .|F----7|.
+            .||....||.
+            .||....||.
+            .||....||.
+            .|L-7F-J|.
+            .|..||..|.
+            .L--JL--J.
+            ..........
+            """, 4),
+
+    (
+            # tiles inside that don't touch the loop
+            """
+            .........
+            .F--S--7.
+            .|.....|.
+            .|.....|.
+            .|.....|.
+            .L-----J.
+            """, 15),
+])
+def test_problem2(text, expected):
+    assert problem2(text.strip().replace(" ", "")) == expected
+
+
+def test_problem2_examples():
+    assert_examples(problem2, examples=range(2, 4, 5))
