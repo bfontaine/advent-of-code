a1408191b1b75b0fc4f46570f58a8bf5647b08c9
Baptiste Fontaine <b@ptistefontaine.fr>
2023-12-23 18:11:22 +0100

    day 12: start over for part 2

--- aoc2023/day12.py
+++ aoc2023/day12.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Iterable
+from typing import List
 
 import aoc
 
@@ -7,21 +7,6 @@ import aoc
 EMPTY = "."  # = operational
 PRESENT = "#"  # = damaged
 UNKNOWN = "?"
-
-
-def _product(all_positions: List[List[int]], groups: List[int]) -> Iterable[List[int]]:
-    partial_groups_positions: Iterable[List[int]] = [[]]
-
-    for i, group_positions in enumerate(all_positions):
-        partial_groups_positions = [
-            partial_group_positions + [position]
-            for partial_group_positions in partial_groups_positions
-            for position in group_positions
-            if i == 0 or (
-                    partial_group_positions[-1] + groups[i - 1] < position
-            )
-        ]
-    return partial_groups_positions
 
 
 @dataclass
@@ -50,169 +35,25 @@ class Record:
     def __str__(self):
         return self._str
 
-    def get_raw_possible_group_positions(self) -> List[List[int]]:
-        """
-        Return a list of ranges representing the possible starting positions of each group.
-        This needs to be filtered down because it includes impossible arrangements.
-        """
-        group_position_ranges: List[List[int]] = []
-        min_x = 0
-
-        starts_with_present = self.conditions[0] == PRESENT
-        ends_with_present = self.conditions[-1] == PRESENT
-
-        for i, group in enumerate(self.groups):
-            # Special case when the first position is 'present'
-            if starts_with_present and i == 0:
-                group_position_ranges.append([0])
-                min_x += group + 1
-                continue
-
-            # idem for the last one
-            if ends_with_present and i == len(self.groups) - 1:
-                group_position_ranges.append([self.size - group])
-                continue
-
-            right_groups = self.groups[i + 1:]
-
-            right_margin = sum(right_groups) + len(right_groups)
-            max_x = self.size - right_margin - group
-
-            # in the examples below, group=3
-
-            # [.???] -> .[???]
-            # [??.???] -> ??.[???]
-            while EMPTY in self.conditions[min_x:min_x + group]:
-                min_x += 1
-
-            # [???.] -> [???].
-            # [???.?] -> [???].?
-            while EMPTY in self.conditions[max_x:max_x + group]:
-                max_x -= 1
-
-            r = [
-                x
-                for x in range(min_x, max_x + 1)
-                if EMPTY not in self.conditions[x:x + group]
-            ]
-
-            group_position_ranges.append(r)
-            min_x += group + 1
-
-        return group_position_ranges
-
-    def _filter_possible_group_positions(self, r: Iterable[int], group: int):
-        """
-        Given a range of x's and a group index, return the valid x's the group can start at.
-        """
-        valid_xs: List[int] = []
-        for x in r:
-            if EMPTY in self.conditions[x:x + group]:
-                continue
-
-            valid_xs.append(x)
-
-        return valid_xs
-
-    def _reduce_possible_group_positions(self, group_positions: List[List[int]]):
-        changed = True
-        while changed:
-            changed = False
-            # reduce the possibilities:
-            for i in range(len(self.groups)):
-                if i > 0:
-                    # min_x of a group must be >= min_x of the group before + length of that group + 1
-                    prev_group = self.groups[i - 1]
-                    if prev_group_positions := group_positions[i - 1]:
-                        prev_min_x = prev_group_positions[0]
-                        new_positions = [p for p in group_positions[i] if p > prev_min_x + prev_group]
-                        if group_positions[i] != new_positions:
-                            changed = True
-                            group_positions[i] = new_positions
-
-                if i < len(self.groups) - 1:
-                    # max_x of a group must be <= max_x of the group after - length of the current group - 1
-                    group = self.groups[i]
-                    if next_group_positions := group_positions[i + 1]:
-                        next_max_x = next_group_positions[-1]
-                        new_positions = [p for p in group_positions[i] if p < next_max_x - group]
-                        if group_positions[i] != new_positions:
-                            changed = True
-                            group_positions[i] = new_positions
-
-    def get_possible_group_positions(self) -> List[List[int]]:
-        """
-        Return a list where [i] contains the possible x's the group[i] can start at.
-        """
-        group_positions: List[List[int]] = []
-        for i, r in enumerate(self.get_raw_possible_group_positions()):
-            conditions = self._filter_possible_group_positions(r, self.groups[i])
-            group_positions.append(conditions)
-
-        assert len(group_positions) == len(self.groups)
-
-        self._reduce_possible_group_positions(group_positions)
-
-        return group_positions
-
-    def _is_arrangement_possible(self, arrangement: List[int]):
-        arrangement = arrangement + [self.size + 1]
-
-        for i, x in enumerate(arrangement):
-            if i == len(arrangement) - 1:
-                break
-
-            # no 'present' between the beginning of the record and the first group
-            if i == 0 and PRESENT in self.conditions[:x]:
-                return False
-
-            # No 'present' before the group
-            if x > 0 and self.conditions[x - 1] == PRESENT:
-                return False
-
-            x_after_group = x + self.groups[i]
-
-            # no overlap between groups
-            if x_after_group + 1 > arrangement[i + 1]:
-                return False
-
-            # no 'present' between groups
-            if PRESENT in self.conditions[x_after_group: arrangement[i + 1]]:
-                return False
-
-        return True
-
     def count_possible_arrangements(self) -> int:
-        possible_group_positions = self.get_possible_group_positions()
-        total = 0
-
-        # print(possible_group_positions)
-        for arrangement in _product(possible_group_positions, self.groups):
-            if self._is_arrangement_possible(arrangement):
-                total += 1
-
-        return total
+        return 42  # TODO
 
 
-def problem1(text: str):
+def count_all_possible_arrangements(text: str, folds=1):
     total = 0
     for line in text.splitlines(keepends=False):
-        record = Record.from_string(line)
+        record = Record.from_string(line, folds=folds)
         total += record.count_possible_arrangements()
 
     return total
 
 
+def problem1(text: str):
+    return count_all_possible_arrangements(text)
+
+
 def problem2(text: str):
-    total = 0
-
-    for line in text.splitlines(keepends=False):
-        print(line)
-        f5 = Record.from_string(line, folds=5).count_possible_arrangements()  # XXX not fast enough
-
-        total += f5
-
-    return total
+    return count_all_possible_arrangements(text, folds=5)
 
 
 if __name__ == '__main__':
--- aoc2023/tests/test_day12.py
+++ aoc2023/tests/test_day12.py
@@ -1,105 +1,7 @@
-import json
-
 import pytest
 
 from aoc import assert_examples, get_input_data
-from day12 import problem1, problem2, Record, _product
-
-
-@pytest.mark.parametrize("record_str,expected1", [
-    ("### 3", [0]),
-    ("#??? 2", [0]),
-    ("???? 4", [0]),
-    ("???? 3", [0, 1]),
-    ("???? 2", [0, 1, 2]),
-    ("???? 1", [0, 1, 2, 3]),
-    (".??? 1", [1, 2, 3]),
-    ("..?? 1", [2, 3]),
-    ("..?? 2", [2]),
-    ("?? 2", [0]),
-    ("?? 1", [0, 1]),
-    ("??.. 2", [0]),
-    ("??.. 1", [0, 1]),
-])
-def test_get_raw_possible_group_positions1(record_str, expected1):
-    r = Record.from_string(record_str)
-    assert r.get_raw_possible_group_positions() == [expected1]
-
-
-@pytest.mark.parametrize("record_str,expected", [
-    ("###?? 3,1", [[0], [4]]),
-    ("??### 1,3", [[0], [2]]),
-    ("???### 1,3", [[0, 1], [3]]),
-    ("???.????? 3,1", [[0, 4], [4, 5, 6, 7, 8]]),
-])
-def test_get_raw_possible_group_positions2(record_str, expected):
-    r = Record.from_string(record_str)
-    assert r.get_raw_possible_group_positions() == expected
-
-
-@pytest.mark.parametrize("record_str,expected", [
-    # ??????#.???#????.???????#.???#????.
-    # 0123456 89012345 78901234 678901234
-    #           1           2         3
-    ("??????#.???#????. 1,1,2,4,1",
-     json.loads("""
-     [[0, 1, 2, 3, 4],
-            [2, 3, 4, 5, 6],
-                  [4, 5,       8, 9],
-                              [8, 9, 10, 11, 12],
-                                                [13, 14, 15,     17],
-                                                        [15,     17, 18, 19],
-                                                                [17, 18, 19, 20, 21],
-                                                                        [19, 20, 21, 22, 23],
-                                                                                                    [26, 27, 28],
-                                                                                                                        [31, 32, 33]]
-     """)),
-])
-def test_get_possible_group_positions_fold2(record_str, expected):
-    r = Record.from_string(record_str, folds=2)
-    assert r.get_possible_group_positions() == expected
-
-
-@pytest.mark.parametrize("record_str,expected", [
-    ("???.??? 3", [[0, 4]]),
-    ("???.??????? 1,4", [[0, 1, 2, 4, 5], [4, 5, 6, 7]]),
-])
-def test_get_possible_group_positions_unchanged_raw(record_str, expected):
-    r = Record.from_string(record_str)
-    assert r.get_raw_possible_group_positions() == expected
-    assert r.get_possible_group_positions() == expected
-
-
-"""
-@pytest.mark.parametrize("record_str,expected", [
-    ("??? 1", [(0,), (1,), (2,)]),
-    ("??? 1,1", [(0, 2)]),
-    ("????? 2,2", [(0, 3)]),
-    ("?????? 2,2", [(0, 3), (0, 4), (1, 4)]),
-    ("#?? 1,1", [(0, 2)]),
-    ("???.??? 3", [(0,), (4,)]),
-    ("???.??? 2", [(0,), (1,), (4,), (5,)]),
-    ("???.### 1,1,3", [(0, 2, 4)]),
-    (".??..??...?##. 1,1,3", [(1, 5, 10), (1, 6, 10), (2, 5, 10), (2, 6, 10)]),
-
-    ("?#?#?#?#?#?#?#? 1,3,1,6", [(1, 3, 7, 9)]),
-    ("?#?#?#?#?#?#?#?.... 1,3,1,6", [(1, 3, 7, 9)]),
-    ("?#.#?#?#?#?#?#? 1,3,1,6", [(1, 3, 7, 9)]),
-
-    ("??##??##?? 2,2", [(2, 6)]),
-
-    ("???.??????? 1,4",
-     [(x, y)
-      for x in (0, 1, 2)
-      for y in (4, 5, 6, 7)] + [(4, 6), (4, 7), (5, 7)]),
-
-    ("?????...#?? 5,1", [(0, 8)]),
-    (".?#???..????#. 1,5", [(2, 8)]),
-])
-def test_get_possible_arrangements(record_str, expected):
-    r = Record.from_string(record_str)
-    assert sorted(r.get_possible_arrangements()) == expected
-"""
+from day12 import problem1, problem2, Record
 
 
 @pytest.mark.parametrize("record_str,expected", [
@@ -137,28 +39,6 @@ def test_count_possible_arrangements_folds2(record_str, expected1, expected2):
     assert Record.from_string(record_str, folds=2).count_possible_arrangements() == expected2
 
 
-@pytest.mark.parametrize("possible_positions,groups,expected", [
-    ([[0, 1, 2, 3], [2, 3, 4]], [1, 1], [
-        [0, 2], [0, 3], [0, 4],
-        [1, 3], [1, 4],
-        [2, 4]
-    ]),
-    ([[0, 1, 2], [3], [10]], [2, 5, 25], [
-        [0, 3, 10]
-    ]),
-    ([[0, 1, 2, 3], [3, 4], [4, 5, 6, 7]], [3, 1, 1],
-     [[0, 4, 6], [0, 4, 7]]),
-
-    ([[0, 1, 2, 3], [3, 4], [4, 5, 6]], [2, 1, 1],
-     [[0, 3, 5],
-      [0, 3, 6],
-      [0, 4, 6],
-      [1, 4, 6]]),
-])
-def test_product(possible_positions, groups, expected):
-    assert _product(possible_positions, groups) == expected
-
-
 def test_problem1_examples():
     assert_examples(problem1)
 
@@ -171,6 +51,9 @@ def test_problem1_input():
     ("???.### 1,1,3", 1),
     (".??..??...?##. 1,1,3", 16384),
     ("?#?#?#?#?#?#?#? 1,3,1,6", 1),
+    ("????.#...#... 4,1,1", 16),
+    ("????.######..#####. 1,6,5", 2500),
+    ("?###???????? 3,2,1", 506250),
 ])
 def test_problem2_detailed_examples(text, expected):
     assert problem2(text) == expected
